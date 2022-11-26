389281e86c97a993bd459da0aa8fdcc4fb3e6d0c
tkz96 <tkz9696@gmail.com>
2022-11-26 19:07:02 +0500

    Add splash screen routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
+  get 'groups/index', as: :user_root
+
   # Defines the root path route ("/")
-  root "groups#index"
+  root "static_pages#splash"
 
   resources :users, :proceedings, :groups
 end
